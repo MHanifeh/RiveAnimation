7e5468ac1134a0a4f2fc1c5149e29997f987b15e
Mahdi <97763355+MHanifeh@users.noreply.github.com>
2022-12-27 19:34:40 +0330

    Initial Commit

---
+++ RiveAnimation/RiveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationApp.swift
+//  RiveAnimation
+//
+//  Created by MahdiHanifeh on 10/6/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
